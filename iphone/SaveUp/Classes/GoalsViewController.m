f1296e26f927ab63188b52325b2bf53847f95fac
Clint Shryock <clint@ctshryock.com>
2010-11-19 07:59:37 -0600

    Stoped tracking two files.

--- iphone/SaveUp/Classes/GoalsViewController.m
+++ iphone/SaveUp/Classes/GoalsViewController.m
@@ -102,19 +102,16 @@
 */
 
 
-/*
-// Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source.
+		[goals removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }   
+	[tableView endUpdates];
 }
-*/
+
 
 
 /*
